38024732912eb4c3e52e861c9944ff55e292226c
0xatanda <atanda0x@gmail.com>
2025-02-04 01:58:09 -0800

    utils fix

--- internal/mathutils/properties.go
+++ internal/mathutils/properties.go
@@ -18,49 +18,42 @@ func IsPrime(n int) bool {
 	return true
 }
 
+
 func IsPerfect(n int) bool {
 	if n < 2 {
 		return false
 	}
 	sum := 1
-	limit := int(math.Sqrt(float64(n)))
-	for i := 2; i <= limit; i++ {
+	for i := 2; i <= n/2; i++ {
 		if n%i == 0 {
 			sum += i
-			otherDiv := n / i
-			if otherDiv != 1 {
-				sum += otherDiv
-			}
 		}
 	}
 	return sum == n
 }
 
-func IsArmStrong(n int) bool {
-	absNum := n
-	if n < 0 {
-		absNum = -n
-	}
+func IsArmstrong(n int) bool {
+	absNum := int(math.Abs(float64(n)))
 	str := strconv.Itoa(absNum)
 	power := len(str)
 	sum := 0
+
 	for _, ch := range str {
 		digit := int(ch - '0')
 		sum += int(math.Pow(float64(digit), float64(power)))
 	}
+
 	return sum == absNum
 }
 
+
 func DigitSum(n int) int {
-	absNum := n
-	if n < 0 {
-		absNum = -n
-	}
+	n = int(math.Abs(float64(n))) 
 	sum := 0
 
-	for absNum != 0 {
-		sum += absNum % 10
-		absNum /= 10
+	for n > 0 {
+		sum += n % 10
+		n /= 10
 	}
 	return sum
 }
